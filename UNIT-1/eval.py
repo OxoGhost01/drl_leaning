345d8162eb1fb1f4e886ff96cc609736f8746843
OxoGhost <oxoghost@DEBIANBOT-2000.home>
2025-06-17 18:36:59 +0200

    trying with 2nd model

--- UNIT-1/eval.py
+++ UNIT-1/eval.py
@@ -4,7 +4,7 @@ from stable_baselines3.common.evaluation import evaluate_policy
 from stable_baselines3 import PPO
 
 
-model = PPO.load("UNIT-1/ppo_lunarlander-v2")
+model = PPO.load("UNIT-1/ppo_lunarlander-v2.2")
 eval_env = Monitor(gym.make("LunarLander-v2"))
 mean_reward, std_reward = evaluate_policy(model, eval_env, n_eval_episodes=10, deterministic=True)
 print(f"mean_reward={mean_reward:.2f} +/- {std_reward}")
